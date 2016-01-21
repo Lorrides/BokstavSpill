2f8815bef4015ea67ab83c6cf639b266078f3052
Lorrides <tarhh@outlook.com>
2016-01-21 12:49:50 +0100

    Lagt til logikk for tidsintervaler for vanskelighetsgrad. Tilgitt Correct Missed Total felt verdier.

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,11 @@ namespace BokstavSpill2
 {
     public partial class Form1 : Form
     {
+
+
+        Random random = new Random();
+        Stats stats = new Stats();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +24,52 @@ namespace BokstavSpill2
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Legger til en random nøkkel til listBoxen
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7 )
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //If the uesr pressed a key that's in the ListBox, remove it
+            //and then make the game alittle faster
+            if ( listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+
+                //The user pressd a correct key, so update the stats object
+                //by calling its update() method with the argument true
+                stats.Update(true);
+                }
+            else
+            {
+                //the user prossed an incorrect key, so update the Stas object
+                //by calling its Update() method with tha argument false
+                stats.Update(false);
+            }
+            //uppdate the labels on the StatusStrip
+            correctLabel.Text = "Correct: " + stats.Correct;
+            missedLabel.Text = "Missed: " + stats.Missed;
+            totalLabel.Text = "Total: " + stats.Total;
+            accuracyLabel.Text = "Accuracy: " + stats.Accuracy +"%";
 
         }
     }
